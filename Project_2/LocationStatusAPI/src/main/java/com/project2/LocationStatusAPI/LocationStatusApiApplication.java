ac3e5ca237de5ce02821ba16a33f9326f19ad31a
HarrisonFok <harrison.fok@gmail.com>
2022-05-09 17:14:50 -0400

    Restructured the apps so they could be run

---
+++ Project_2/LocationStatusAPI/src/main/java/com/project2/LocationStatusAPI/LocationStatusApiApplication.java
@@ -0,0 +1,13 @@
+package com.project2.LocationStatusAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationStatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationStatusApiApplication.class, args);
+	}
+
+}
---
+++ Project_2/LocationStatusAPI/src/test/java/com/project2/LocationStatusAPI/LocationStatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.LocationStatusAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationStatusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
