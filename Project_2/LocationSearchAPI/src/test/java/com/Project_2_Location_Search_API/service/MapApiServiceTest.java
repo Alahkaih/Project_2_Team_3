f27b6248744082dc69406c6c0b87e3e787cbfc4a
K.J <75954736+8080co@users.noreply.github.com>
2022-05-21 23:25:24 -0400

    shouldFetchRequest test created and passed

--- Project_2/LocationSearchAPI/src/test/java/com/Project_2_Location_Search_API/service/MapApiServiceTest.java
+++ Project_2/LocationSearchAPI/src/test/java/com/Project_2_Location_Search_API/service/MapApiServiceTest.java
@@ -1,5 +1,6 @@
 package com.Project_2_Location_Search_API.service;
 
+import com.Project_2_Location_Search_API.dto.CoordinatesDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,28 +20,47 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class MapApiServiceTest {
-//    @Mock
-//    private RestTemplate restTemplate;
-//
-//    @InjectMocks
-//    private MapService mapService;
-//
-//    @Test
-//    public void shouldFetchRequest(){
-//
-//        String url = "http://localhost:8081/map/state-info?state=Texas&format=json&countrycodes=us";
-//
-//
-//        ResponseEntity<String> responseEntity = new ResponseEntity<>(url, HttpStatus.OK);
-//        when(restTemplate.exchange(
-//                ArgumentMatchers.anyString(),
-//                ArgumentMatchers.any(HttpMethod.class),
-//                ArgumentMatchers.any(),
-//                ArgumentMatchers.<Class<String>>any()))
-//                .thenReturn(responseEntity);
-//
-//        ResponseEntity response = mapService.fetchRequest(url);
-//        Assertions.assertEquals(responseEntity.getBody(), response.getBody());
-//    }
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private MapService mapService;
+
+    @Test
+    public void shouldFetchRequest(){
+
+        String url = "http://localhost:8081/map/state-info?state=Texas&format=json&countrycodes=us";
+
+        CoordinatesDTO coordinatesDTO = new CoordinatesDTO("-98.5456116","31.2638905");
+        ResponseEntity<CoordinatesDTO> responseEntity = new ResponseEntity<>(coordinatesDTO, HttpStatus.OK);
+        when(restTemplate.exchange(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.any(HttpMethod.class),
+                ArgumentMatchers.any(),
+                ArgumentMatchers.<Class<CoordinatesDTO>>any()))
+                .thenReturn(responseEntity);
+
+        ResponseEntity response = mapService.fetchRequest(url);
+        Assertions.assertEquals(responseEntity.getStatusCodeValue(), response.getStatusCodeValue());
+    }
+/*
+    @Test
+    public void shouldFetchImage(){
+
+        String url = "http://localhost:8081/map/Canada";
+
+        CoordinatesDTO coordinatesDTO = new CoordinatesDTO("-98.5456116","31.2638905");
+        ResponseEntity<CoordinatesDTO> responseEntity = new ResponseEntity<>(coordinatesDTO, HttpStatus.OK);
+        when(restTemplate.exchange(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.any(HttpMethod.class),
+                ArgumentMatchers.any(),
+                ArgumentMatchers.<Class<CoordinatesDTO>>any()))
+                .thenReturn(responseEntity);
+
+        ResponseEntity response = mapService.fetchImage(url);
+        Assertions.assertEquals(responseEntity.getStatusCodeValue(), response.getStatusCodeValue());
+    }
+*/
 
 }
